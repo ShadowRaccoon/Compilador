533657e22cf392749cb61cf84dde79d8d71d64ca
Lautaro Carlos <ldercole@alumno.unlam.edu.ar>
2022-02-12 06:01:16 -0300

    Actualización bat

--- script.bat
+++ script.bat
@@ -2,7 +2,10 @@
 C:\GnuWin32\bin\flex "C:\Users\rocio\OneDrive\Escritorio\Lenguajes y Compiladores\Lexico.l"
 echo "FLEX Compilo la especificacion lexica del archivo Lexico.l (Se creo un archivo lex.yy.c)"
 pause
-C:\MinGW\bin\gcc.exe lex.yy.c -o Compilado.exe
+C:\GnuWin32\bin\bison -dyv "C:\Users\rocio\OneDrive\Escritorio\Lenguajes y Compiladores\Sintactico.y"
+echo "FLEX Compilo la especificacion lexica del archivo Lexico.l (Se creo un archivo lex.yy.c)"
+pause
+C:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o Compilado.exe
 echo "Finalizo la ejecucion del compilador de C sobre el archivo lex.yy.c (Se creo un archivo Compilado.exe)"
 pause
 Compilado.exe Prueba.txt
